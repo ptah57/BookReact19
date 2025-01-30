8f27a3ef8f8b266590ffb4271ad5de121e675aac
ptah57 <ptah57@gmail.com>
2025-01-30 18:56:38 +0300

    Update routes.js
    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,4 @@
-import { createRoutesFromElements, createBrowserRouter, Route } from
- 'react-router-dom';
+import { createRoutesFromElements, createBrowserRouter, Route } from 'react-router-dom';
 
 import App from './App.js';
 import TodoList from './TodoList.js';
